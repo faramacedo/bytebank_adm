7370706d54824e58379f7d80cc40b46e97df2bac
Fabio Macedo <faramacedo@gmail.com>
2022-12-31 09:53:20 -0300

    Sobrecarga de método

---
+++ bytebank_adm/Funcionarios/GerenciadorDeBonificacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_adm.Funcionarios
+{
+    public class GerenciadorDeBonificacao
+    {
+        public double TotalDeBonificacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalDeBonificacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            this.TotalDeBonificacao += diretor.GetBonificacao();
+        }
+    }
+}
+
--- bytebank_adm/Program.cs
+++ bytebank_adm/Program.cs
@@ -16,3 +16,9 @@ roberta.Salario = 5000;
 
 Console.WriteLine(roberta.Nome);
 Console.WriteLine(roberta.GetBonificacao());
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine($"Total de bonificações: R$ {gerenciador.TotalDeBonificacao}");
