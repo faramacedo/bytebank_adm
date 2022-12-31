436deb3242a41f6f938b8bdf6b454c114dc291a6
Fabio Macedo <faramacedo@gmail.com>
2022-12-31 09:40:45 -0300

    Exemplo Classes Funcionario e Diretor sujo

--- bytebank_adm/Program.cs
+++ bytebank_adm/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bytebank_adm.Funcionarios;
+
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro Malazartes";
+pedro.Cpf = "123456789";
+pedro.Salario = 2000;
+
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.GetBonificacao());
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Silva";
+roberta.Cpf = "987654321";
+roberta.Salario = 5000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
